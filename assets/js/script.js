0ca84fd183286464b2e8354d9c8aacba4f073063
John Shrubb <34843796+shrubster@users.noreply.github.com>
2022-09-13 21:00:43 +0000

    Further improvement to script to avoid hardcoded values when adjusting for navbar height that are unpredictable and could break.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,6 @@
-const navHeight = document.getElementsByClassName('nav-bar')[0].offsetHeight + 9;
-document.getElementsByClassName('welcome-block')[0].style.paddingTop = navHeight + 'px';
+// Dynamically adjust rest of page for navigation bar's height.
+
+window.onload = () => {
+	const navHeight = document.getElementsByClassName('nav-bar')[0].offsetHeight;
+	document.getElementsByClassName('welcome-block')[0].style.paddingTop = navHeight + 'px';
+}
