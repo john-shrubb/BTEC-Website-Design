df2e44c3d1ebe24c5582df0145fc486ce7719dfd
John Shrubb <34843796+shrubster@users.noreply.github.com>
2022-09-14 19:20:43 +0000

    Complete most of welcome block. Easier than I thought tbh.

--- assets/js/welcome-block.js
+++ assets/js/welcome-block.js
@@ -1,8 +1,19 @@
 // This script adds changing background pictures on the div.welcome-block element in ../../index.html
 // Should not be imported into other pages otherwise it may cause errors.
 
-const picturesToCyclethrough = ['../images/welcome-block-1.jpg', '../images/welcome-block-2.webp'];
+const picturesToCyclethrough = ['welcome-block-1.jpg', 'welcome-block-2.webp'];
+let currentPictureIndex = 0;
+
+const welcomeBlock = document.getElementsByClassName('welcome-block')[0];
+welcomeBlock.style.backgroundImage = 'url(\'./assets/images/' + picturesToCyclethrough[0] + '\')';
 
 setInterval(() => {
-	console.log('urah');
+	currentPictureIndex++;
+
+	if (currentPictureIndex === picturesToCyclethrough.length) {
+		currentPictureIndex = 0;
+	}
+
+
+	welcomeBlock.style.backgroundImage = 'url(\'./assets/images/' + picturesToCyclethrough[currentPictureIndex] + '\')';
 }, 10000);
