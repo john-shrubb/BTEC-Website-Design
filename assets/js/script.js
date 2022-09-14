baf3412ec48203e4b43861a0fa2816f8fda3d1a1
John Shrubb <34843796+shrubster@users.noreply.github.com>
2022-09-14 18:02:17 +0000

    Sort hamburger menu icon

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,53 @@
 // Dynamically adjust rest of page for navigation bar's height.
 
 window.onload = () => {
+	window.onresize();
+	// navHeight is the height of the navigation bar
+
 	const navHeight = document.getElementsByClassName('nav-bar')[0].offsetHeight;
+
+	// Dynamically make sure that rest of content is just below navigation bar
+
 	document.getElementsByClassName('welcome-block')[0].style.paddingTop = navHeight + 'px';
-}
+
+	// Dynamically make hamburger menu icon the height of the navigation bar
+
+	const navHamMenuIcon = document.getElementById('nav-ham-menu').children[0];
+
+	navHamMenuIcon.style.height = navHeight + 'px';
+	document.getElementById('nav-ham-menu').style.height = navHeight + 'px';
+};
+
+window.onresize = () => {
+	const height = window.innerHeight;
+	const width = window.innerWidth;
+
+	// Links shown on a device below 600 pixels in width
+
+	const hamburgerMenuButton = document.getElementById('nav-ham-menu');
+	const hamburgerMenuLinks = document.getElementsByClassName('mobile-a-wrapper')[0];
+
+	// Links shown on a device above 600 pixels in width
+
+	const desktopLinks = document.getElementsByClassName('a-wrapper')[0];
+
+	if (width <= 600) {
+		// Show hamburger menu
+
+		hamburgerMenuButton.style.display = "inline-block";
+		hamburgerMenuLinks.style.display = "inline-block";
+
+		// Hide desktop links
+
+		desktopLinks.style.display = 'none';
+	} else {
+		// Hide hamburger menu
+
+		hamburgerMenuButton.style.display = 'none';
+		hamburgerMenuLinks.style.display = 'none';
+
+		// Show desktop links
+
+		desktopLinks.style.display = 'flex';
+	}
+};
--- doc/credits.md
+++ doc/credits.md
@@ -6,3 +6,4 @@ This document contains credits for all services used throughout the creation of 
 - [Google Maps Embed](https://googlemapsembed.com) & [Google Maps](https://maps.google.com) for providing embedded Google Maps.
 - [Meyerweb](https://meyerweb.com/) for [CSS pallete reset](https://meyerweb.com/eric/tools/css/reset/).
 - [Google Fonts](https://fonts.google.com) as all non built in fonts were sourced from Google Fonts.
+- [icons8.com](https://icons8.com) for hamburger menu icon
