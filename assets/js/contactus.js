80645cbc2bec3231e3b693fbc1e800798e5d3522
John Shrubb <34843796+shrubster@users.noreply.github.com>
2022-09-14 21:26:47 +0000

    Mostly add mobile compatability (Still some work but meh)

--- assets/js/contactus.js
+++ assets/js/contactus.js
@@ -2,8 +2,28 @@ const contactSubmitButton = document.querySelector('button#contactSubmitButton')
 
 const clearForm = () => {
 	const inputs = document.querySelector('form>div.inputs').children;
+	const inputsMobile = document.querySelector('.mobile-form').children;
 
 	for (let x = 0; x < inputs.length; x++) {
 		inputs[x].value = '';
 	}
+
+	for (let x = 0; x < inputsMobile.length; x++) {
+		inputsMobile[x].value = '';
+	}
 };
+
+// Mirror mobile form and desktop form
+
+window.addEventListener('keypress', () => {
+	if (window.innerWidth > 600) {
+		document.querySelector('#yournamem').value = document.querySelector('#yourname').value;
+		document.querySelector('#youremailm').value = document.querySelector('#youremail').value;
+		document.querySelector('#yourmessagem').value = document.querySelector('#yourmessage').value;
+	} else {
+		document.querySelector('#yourname').value = document.querySelector('#yournamem').value;
+		document.querySelector('#youremail').value = document.querySelector('#youremailm').value;
+		document.querySelector('#yourmessage').value = document.querySelector('#yourmessagem').value;
+
+	}
+});
