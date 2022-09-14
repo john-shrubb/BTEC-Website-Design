922626bd6a58f0256e92c4d6f8a81a7c227fa608
John Shrubb <34843796+shrubster@users.noreply.github.com>
2022-09-14 19:53:46 +0000

    Peformance improvements. Welcome block images now preloaded before display to make transition more instant.

--- assets/js/welcome-block.js
+++ assets/js/welcome-block.js
@@ -1,11 +1,23 @@
 // This script adds changing background pictures on the div.welcome-block element in ../../index.html
-// Should not be imported into other pages otherwise it may cause errors.
+// Should not be imported into pages other than index.html.
 
-const picturesToCyclethrough = ['welcome-block-1.jpg', 'welcome-block-2.webp'];
+const picturesToCyclethrough = ['./assets/images/welcome-block-1.jpg', './assets/images/welcome-block-2.webp'];
 let currentPictureIndex = 0;
 
+// Preload all images
+
+function preloadImage(imageURL) {
+	const image = new Image();
+	image.src = imageURL;
+	return true;
+}
+
+picturesToCyclethrough.forEach(preloadImage);
+
+// Set welcome block background to index 0 in pictures array.
+
 const welcomeBlock = document.getElementsByClassName('welcome-block')[0];
-welcomeBlock.style.backgroundImage = 'url(\'./assets/images/' + picturesToCyclethrough[0] + '\')';
+welcomeBlock.style.backgroundImage = 'url(\'' + picturesToCyclethrough[0] + '\')';
 
 setInterval(() => {
 	currentPictureIndex++;
@@ -14,6 +26,6 @@ setInterval(() => {
 		currentPictureIndex = 0;
 	}
 
-
-	welcomeBlock.style.backgroundImage = 'url(\'./assets/images/' + picturesToCyclethrough[currentPictureIndex] + '\')';
+	// e.g url('./assets/images/welcome-block-1.jpg')
+	welcomeBlock.style.backgroundImage = `url('${picturesToCyclethrough[currentPictureIndex]}')`;
 }, 10000);
