da56f9535a9f73181387866a9a06dfb1897c4801
John Shrubb <34843796+shrubster@users.noreply.github.com>
2022-09-14 18:59:16 +0000

    Begin welcome block, add reference and rename & add standard welcome block image name

---
+++ assets/js/welcome-block.js
@@ -0,0 +1,8 @@
+// This script adds changing background pictures on the div.welcome-block element in ../../index.html
+// Should not be imported into other pages otherwise it may cause errors.
+
+const picturesToCyclethrough = ['../images/welcome-block-1.jpg', '../images/welcome-block-2.webp'];
+
+setInterval(() => {
+	console.log('urah');
+}, 10000);
